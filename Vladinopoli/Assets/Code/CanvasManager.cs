2c0be047cdb6200fc3f7ab1c8310a30db60f0799
kostaivanov <kostasvetlozarov@gmail.com>
2021-09-23 01:02:26 +0300

    Update Code.meta, CanvasController.cs, and 11 more files...

---
+++ Vladinopoli/Assets/Code/CanvasManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class CanvasManager : MonoBehaviour
+{
+    private List<CanvasController> canvasControllerList;
+    private CanvasController lastActiveCanvas;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        canvasControllerList = GetComponentsInChildren<CanvasController>().ToList();
+        canvasControllerList.ForEach(x => x.gameObject.SetActive(false));
+        SwitchCanvas(CanvasType.Game);
+    }
+
+    private void SwitchCanvas(CanvasType canvasType)
+    {
+        if (lastActiveCanvas != null)
+        {
+            lastActiveCanvas.gameObject.SetActive(false);
+        }
+
+        CanvasController desiredCanvas = canvasControllerList.Find(x => x.canvasType == canvasType);
+
+        if (desiredCanvas != null)
+        {
+            desiredCanvas.gameObject.SetActive(true);
+            lastActiveCanvas = desiredCanvas;
+        }
+        else
+        {
+            Debug.LogWarning("The main menu canvas was not found!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
