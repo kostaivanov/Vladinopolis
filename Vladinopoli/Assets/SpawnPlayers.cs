43dc38964bf1531a0447f4f61bcc16fc71724e15
kostaivanov <kostasvetlozarov@gmail.com>
2021-09-27 23:34:13 +0300

    Update SpawnPlayers.cs

--- Vladinopoli/Assets/SpawnPlayers.cs
+++ Vladinopoli/Assets/SpawnPlayers.cs
@@ -15,21 +15,22 @@ public class SpawnPlayers : MonoBehaviour
 
     private void Start()
     {
+        camera.enabled = false;
         if (StaticFunctions.instance.name == "Rumen".Trim())
         {
             //Vector3 randomPosition = new Vector3(Random.Range(min_X, max_X), 36.76f, Random.Range(min_Z, max_Z));
             Vector3 randomPosition = new Vector3(75f, 40f, 90f);
             
 
-            GameObject rumen = PhotonNetwork.Instantiate(rumenPrefab.name, randomPosition, Quaternion.identity);
-            rumen.transform.localEulerAngles = new Vector3(-90f, 50f, 180f);
+            GameObject rumen = PhotonNetwork.Instantiate(rumenPrefab.name, randomPosition, Quaternion.Euler(-90, 50, 180));
+            //rumen.transform.localEulerAngles = new Vector3(-90f, 50f, 180f);
         }
         else
         {
             //Vector3 randomPosition = new Vector3(Random.Range(min_X, max_X), 36.76f, Random.Range(min_Z, max_Z));
             Vector3 randomPosition = new Vector3(65f, 36f, 82f);
-            GameObject kosta = PhotonNetwork.Instantiate(kostaPrefab.name, randomPosition, Quaternion.identity);
-            kosta.transform.localEulerAngles = new Vector3(-90f, 45f, 0f);
+            GameObject kosta = PhotonNetwork.Instantiate(kostaPrefab.name, randomPosition, Quaternion.Euler(-90, 45, 0));
+            //kosta.transform.localEulerAngles = new Vector3(-90f, 45f, 0f);
         }
        
     }
