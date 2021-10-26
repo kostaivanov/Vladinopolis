5e6bf40c588ca4ebad2d5a7df84f887188e50c55
kostaivanov <kostasvetlozarov@gmail.com>
2021-10-27 02:34:19 +0300

    Update PlayerManager.cs and Game.unity

--- Vladinopoli/Assets/Code/PlayerManager.cs
+++ Vladinopoli/Assets/Code/PlayerManager.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Pun.Demo.PunBasics;
-
+using System.Linq;
 public class PlayerManager : MonoBehaviourPun
 {
     [SerializeField] private GameObject camera;
     private GameObject makeYourChoiceObject;
-    private List<GameObject> playerInGame;
+    private List<string> playersInGame;
+
+
+    private int currentNumber;
+    private int previousNumber;
     // Start is called before the first frame update
     void Start()
     {
         CameraWork cameraWork = this.gameObject.GetComponent<CameraWork>();
-        playerInGame = new List<GameObject>();
+        playersInGame = new List<string>();
+
         if (!photonView.IsMine)
         {
             camera.SetActive(false);
@@ -21,6 +26,8 @@ public class PlayerManager : MonoBehaviourPun
 
         makeYourChoiceObject = GameObject.FindGameObjectWithTag("MakeYourChoiceObject");
         makeYourChoiceObject.SetActive(true);
+        currentNumber = PhotonNetwork.CurrentRoom.PlayerCount;
+        //previousNumber = currentNumber;
         //if (cameraWork != null)
         //{
         //    if (photonView.IsMine)
@@ -39,9 +46,11 @@ public class PlayerManager : MonoBehaviourPun
     private void Update()
     {
         Debug.Log("number of player in the game = " + PhotonNetwork.CurrentRoom.PlayerCount);
-        //if (PhotonNetwork.IsConnected)
-        //{
-        //    playerInGame.Add(GameObject.FindGameObjectWithTag("Player"));
-        //}
+        if (currentNumber != PhotonNetwork.CurrentRoom.PlayerCount)
+        {
+            playersInGame.Add(StaticFunctions.instance.name);
+            currentNumber = PhotonNetwork.CurrentRoom.PlayerCount;
+            Debug.Log(playersInGame[0]);
+        }
     }
 }
